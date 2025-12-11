25c3147fa113af65daec5d3f9410c1c98ef41307
DevNayaraVieira <96341147+DevNayaraVieiraa@users.noreply.github.com>
2025-12-11 15:48:08 -0300

    Replace Dev Camila with Dev Nayara in Main.java

--- src/Main.java
+++ src/Main.java
@@ -33,16 +33,16 @@ public class Main {
         bootcamp.getConteudos().add(curso2);
         bootcamp.getConteudos().add(mentoria);
 
-        Dev devCamila = new Dev();
-        devCamila.setNome("Camila");
-        devCamila.inscreverBootcamp(bootcamp);
-        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
-        devCamila.progredir();
-        devCamila.progredir();
+        Dev devNayara = new Dev();
+        devNayara.setNome("Nayara");
+        devNayara.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Nayara:" + devNayara.getConteudosInscritos());
+        devNayara.progredir();
+        devNayara.progredir();
         System.out.println("-");
-        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
-        System.out.println("Conteúdos Concluídos Camila:" + devCamila.getConteudosConcluidos());
-        System.out.println("XP:" + devCamila.calcularTotalXp());
+        System.out.println("Conteúdos Inscritos Nayara:" + deNayara.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Nayara:" + devNayara.getConteudosConcluidos());
+        System.out.println("XP:" + devNayara.calcularTotalXp());
 
         System.out.println("-------");
 
